5d4ffa95631a35b7b463adf86e7731011eba4cc2
KhrapkoVasyl <khrapko2002@gmail.com>
2023-12-10 22:15:27 +0200

    lab7: implement service1 tasks

---
+++ lab-7/service1/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <data-comp></data-comp>
+    <data-comp></data-comp>
+  `,
+})
+export class AppComponent {}
