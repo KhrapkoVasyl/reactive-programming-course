6fea243233b3dcc1ffe2d270ef42e192670a4f36
KhrapkoVasyl <khrapko2002@gmail.com>
2023-12-16 22:53:30 +0200

    lab8: implement routing

--- lab-8/SportShop/src/app/app.module.ts
+++ lab-8/SportShop/src/app/app.module.ts
@@ -3,9 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from './store/store.module';
+import { RouterModule } from '@angular/router';
+import { StoreComponent } from './store/store.component';
+import { CartDetailComponent } from './store/cart-detail.component';
+import { CheckoutComponent } from './store/checkout.component';
 
 @NgModule({
-  imports: [BrowserModule, StoreModule],
+  imports: [
+    BrowserModule,
+    StoreModule,
+    RouterModule.forRoot([
+      { path: 'store', component: StoreComponent },
+      { path: 'cart', component: CartDetailComponent },
+      { path: 'checkout', component: CheckoutComponent },
+      { path: '**', redirectTo: '/store' },
+    ]),
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
